71be198b7318ad716fdd3786a667ab46a9fe24ce
Tristan Trommer <hi@tristantrommer.com>
2025-07-23 22:24:55 +0200

    Integrate with AWS SES

--- actions/email.ts
+++ actions/email.ts
@@ -28,7 +28,7 @@ interface TurnstileValidationResponse {
 }
 
 const aws = new AwsClient({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  accessKeyId: process.env.AWS_ACCESS_KEY || '',
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
   region: process.env.AWS_REGION || ''
 });
