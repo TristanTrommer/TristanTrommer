21a7d57c58f665fdeef8216fcf6d34db191ef8a5
Tristan Trommer <hi@tristantrommer.com>
2026-07-24 12:22:47 +0200

    Add WebSite structured data

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,21 @@ import Projects from '@/components/projects';
 import Contact from '@/components/contact';
 
 export default function Page() {
+  const websiteStructuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Tristan Trommer',
+    url: process.env.NEXT_PUBLIC_URL
+  };
+
   return (
     <div className="pt-28">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(websiteStructuredData)
+        }}
+      />
       <Header />
       <main className="flex flex-col items-center px-4">
         <About />
